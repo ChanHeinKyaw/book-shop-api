b4bbb86a2f0b2e52b12566dd551d9f3a61d64561
ChanHeinKyaw <chanheinkyaw.mm@gmail.com>
2024-03-01 13:45:02 +0630

    user register

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Controllers\Api;
 
+use Throwable;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\RegisterRequest;
 
 class UserController extends Controller
 {
-    //
+    public function store(RegisterRequest $request){
+        try{
+            DB::beginTransaction();
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+            
+            DB::commit();
+
+            return response()->json(['message' => 'User registration success']);
+        }catch(Throwable $th){
+            DB::rollBack();
+            
+            return response()->json([
+                'message' => 'User registration fail',
+            ], 500);
+        }
+    }
 }
